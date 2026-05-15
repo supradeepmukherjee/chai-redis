0eaf89d8923f5847e1d5066dde3ce266ad800cf8
Supradeep Mukherjee <supradeep2004@gmail.com>
2026-05-15 17:53:56 +0530

    Site Banner APIs

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import mongoose from "mongoose";
 import Redis from "ioredis";
 
 const app = express();
+app.use(express.json())
 
 const redis = new Redis("redis://localhost:6379")
+
+const BANNER_KEY = "app:banner"
 
 app.get('/redis', async (req, res) => {
     const _ = await redis.ping()
@@ -14,6 +17,26 @@ app.get('/redis', async (req, res) => {
 app.get('/mongo', async (req, res) => {
     if (mongoose.connection.readyState === 0) await mongoose.connect("mongodb://localhost:27017/redis")
     res.send(mongoose.connection.name)
+})
+
+app.post('/banner', async (req, res) => {
+    await redis.set(BANNER_KEY, req.body.msg || 'Welcome to our website!')
+    res.json({ success: true, })
+})
+
+app.get('/banner', async (req, res) => {
+    const msg = await redis.get(BANNER_KEY)
+    res.json({ msg })
+})
+
+app.delete('/banner', async (req, res) => {
+    await redis.del(BANNER_KEY)
+    res.json({ success: true })
+})
+
+app.get('/banner/exists', async (req, res) => {
+    const exists = await redis.exists(BANNER_KEY)
+    res.json({ exists: Boolean(exists) })
 })
 
 app.listen(6969)
